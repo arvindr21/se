2874cfe0ed3b9d017dc7424148b2d2c131720c2a
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2015-11-02 11:53:18 +0530

    IC

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/arvindravulavaru/Arvind/Org/StackConsulting/yuno/code/scraping-engine/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
